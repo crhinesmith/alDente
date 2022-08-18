2027dc9e82075be0be71d2a940cbb0947b11dc8e
Indigo <indigo.crandell@gmail.com>
2022-08-18 13:16:53 -0400

    still working on search api. I am starting wiht categories. So far, if a user types in a category that matches one of the available categories from our DB, we can trigger a function to show them a few options in search results." "

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -4,26 +4,112 @@ var userInput = document.getElementById("search-text");
 var APIKey = "1";
 var recipeOTDContainer = document.getElementById("recipe-today")
 
-//object for meal of the day
-var mealOTD = {
-    ingredients: [],
-    measurements: [],
-    recipe: "",
-    timeDataWasRetrieved: ""
-}
+    //object for meal of the day
+    var mealOTD = {
+        ingredients: [],
+        measurements: [],
+        recipe: "",
+        timeDataWasRetrieved: ""
+    }
 
 // Working on fetch request with search bar
 
+
+function searchApi(query, genre) {
+    
+    // first, I want to fetch lists of data
+    urlList = 'https://www.themealdb.com/api/json/v1/1/list.php?'
+    listCategories = urlList + 'c=list';
+    listIngredients = urlList + 'i=list';
+    listArea = urlList + 'a=list';
+    console.log(listCategories);
+
+    // fetch category list
+    fetch(listCategories)
+        .then(function (res) {
+            if (!res.ok) {
+                throw res.json(); 
+            }
+            return res.json(); 
+        })
+
+        .then(function (categoryData) {
+            //console.log(categoryData.meals[7].strCategory);
+            //console.log((JSON.stringify(categoryData.meals[7].strCategory)).toLowerCase());
+            var catLength = categoryData.meals.length;
+            // Loop through each category... If user input matches one of the categories, we will use that data in results page... trigger a function to show no more than 10 recipes in that category
+            for (i = 0; i < catLength; i++) {
+            
+                // if the category at index is the same as the user input, console log YEP!
+                if ((JSON.stringify(categoryData.meals[i].strCategory)).toLowerCase() == (JSON.stringify(userQuery)).toLowerCase()) {
+                    console.log("YEP");
+                }
+            }
+        })
+
+
+
+    // URL if user chooses to search by genre
+    if (genre !== '') {
+        recipeUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?c=' + genre;
+    } 
+    // URL if user searches by search bar
+    else {
+         // URL for recipe, without parameters
+        recipeUrl = 'https://www.themealdb.com/api/json/v1/1/search.php';
+
+        // recipeUrl = recipeUrl + parameter + '=' + query
+
+         
+        // if statements to go through parameters
+    /*    // category
+        if () {
+            parameter = 'c'
+        } 
+        // ingredient
+        else if () {
+            parameter = 'i'
+        }
+        // area
+        else if () {
+            parameter = 'a'
+        }
+        // name
+        else if () {
+            parameter = 's'
+        } */
+
+    }
+
+    
+} 
+
 // Define the function triggered by 'submit' in homepage search bar
-function handleSearchForm(e) {
+function handleSearchForm(e){
     // prevent default
-    e.preventDefault();
+    e.preventDefault(); 
+    //create a variable for user input
+    userQuery = userInput.value;
+    //create a variable for genre
+    userGenre = '';
     // On click, log the value typed into the form
     console.log(userInput.value);
+    // Call function to search API and get us requested data
+    searchApi(userQuery, userGenre);
 }
 
 // Event listener, upon search
-searchFormEl.addEventListener('click', handleSearchForm);
+searchFormEl.addEventListener('click', handleSearchForm); 
+
+
+
+
+
+
+
+
+
+
 
 function makeHTML(mealData) {
     console.log(mealData)
@@ -41,59 +127,59 @@ function makeHTML(mealData) {
 
     recipeOTDContainer.append(h1El)
 
-   
-}
-
-function getData() {
-    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            mealOTD.recipe = data.meals[0].strMeal
-            var meal = data.meals[0]
-            for (var e = 1; e < 21; e++) {
-                var ingredient = mealOTD["strIngredient" + e]
-                var measurement = mealOTD["strMeasure" + e]
-                if (ingredient !== null && ingredient !== "") {
-                    mealOTD.ingredients.push(meal["strIngredient" + e])
-                }
-
-                if (measurement !== null && measurement !== "") {
-                    mealOTD.measurements.push(meal["strMeasure" + e])
-                }
-            }
-            var today =  moment().format('YYYY-MM-DD');
-            console.log(today)
-            mealOTD.timeDataWasRetrieved = today
-            localStorage.setItem('mealOfTheDay', JSON.stringify(mealOTD))
-            makeHTML(mealOTD)
-        })
-}
-
-function generateRecipeOTD() {
-    while (recipeOTDContainer.firstChild) {
-        recipeOTDContainer.removeChild(recipeOTDContainer.firstChild)
-    }
-
-    var savedData = localStorage.getItem('mealOfTheDay') // read from local storage
-
     
-    if (savedData !== null) {
-        // use the data already
-        var todaysDate = moment().format('YYYY-MM-DD')
-        if (moment(todaysDate).isAfter(mealOTD.timeDatawasRetrieved)) {
-            getData()
-        } else {
-            // use saved data
-            var parsedData = JSON.parse(savedData)
-            makeHTML(parsedData)
-        }
-    } else {
-        getData()
-    }
 }
-generateRecipeOTD();
+
+    function getData() {
+        fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                mealOTD.recipe = data.meals[0].strMeal
+                var meal = data.meals[0]
+                for (var e = 1; e < 21; e++) {
+                    var ingredient = mealOTD["strIngredient" + e]
+                    var measurement = mealOTD["strMeasure" + e]
+                    if (ingredient !== null && ingredient !== "") {
+                        mealOTD.ingredients.push(meal["strIngredient" + e])
+                    }
+
+                    if (measurement !== null && measurement !== "") {
+                        mealOTD.measurements.push(meal["strMeasure" + e])
+                    }
+                }
+                var today =  moment().format('YYYY-MM-DD');
+                console.log(today)
+                mealOTD.timeDataWasRetrieved = today
+                localStorage.setItem('mealOfTheDay', JSON.stringify(mealOTD))
+                makeHTML(mealOTD)
+            })
+    }
+
+    function generateRecipeOTD() {
+        while (recipeOTDContainer.firstChild) {
+            recipeOTDContainer.removeChild(recipeOTDContainer.firstChild)
+        }
+
+        var savedData = localStorage.getItem('mealOfTheDay') // read from local storage
+
+        
+        if (savedData !== null) {
+            // use the data already
+            var todaysDate = moment().format('YYYY-MM-DD')
+            if (moment(todaysDate).isAfter(mealOTD.timeDatawasRetrieved)) {
+                getData()
+            } else {
+                // use saved data
+                var parsedData = JSON.parse(savedData)
+                makeHTML(parsedData)
+            }
+        } else {
+            getData()
+        }
+    }
+    generateRecipeOTD();
 
 
 //This was in main, question for TA//
