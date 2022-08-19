3131c777db2431676ff0feb6745266f9b7f610a5
samsong1019 <samuelsong18@gmail.com>
2022-08-19 18:03:49 -0400

    new stuff

---
+++ Assets/JS/cookbook.js
@@ -0,0 +1,16 @@
+const recipeList = JSON.parse(localStorage.getItem("recipes")) || []
+console.log(recipeList)
+console.log(recipeList[14]) 
+console.log(recipeList[14].genre)
+
+
+
+
+
+//for (var i = 0; i < recipeList.length; i++) {
+//create table row
+//create table head.
+//append table head to table row. 
+// append table row to table id content
+//change text content
+//}
--- Assets/JS/recipe.js
+++ Assets/JS/recipe.js
@@ -1,3 +1,26 @@
+let currentGenre = ""
+let currentRecipe = ""
+let currentRecipeId = ""
+
+
+function makeList() {
+  var savedRecipe = prompt("would you like to save this recipe?")
+
+  if (savedRecipe = true) {
+    alert("saved to cookbook!");
+  } else {
+    alert("oh bummer"); return null
+  }
+
+  var recipesSaved = confirm();
+
+  var recipesSaved = {
+    savedRecipe: savedRecipe,
+  };
+
+  return recipesSaved
+}
+
 const cookBook = document.getElementById("cookBook");
 const input = document.getElementById("input");
 const recipeSubmit = document.getElementById("recipeSubmit");
@@ -7,29 +30,44 @@ let recipeList = localStorage.getItem("recipes")
   ? JSON.parse(localStorage.getItem("recipes"))
   : [];
 
-  cookBook.addEventListener("submit", (e) => {
-    e.preventDefault();
-    recipeList.push(input.value);
-    localStorage.setItem("recipes", JSON.stringify(recipeList));
-    listBuilder(input.value);
-    input.value = "";
-  });
+cookBook.addEventListener("submit", (e) => {
+  e.preventDefault();
+  currentGenre = "Chicken"
+  currentRecipe = "Chick-Fil-A Sandwich"
+  currentRecipeId = "53016"
+  const recipeObject = {
+    genre: currentGenre,
+    recipe: currentRecipe,
+    recipeId: currentRecipeId,
+  }
+  recipeList.push(recipeObject);
+  localStorage.setItem("recipes", JSON.stringify(recipeList));
+  listBuilder(input.value);
+  input.value = "";
+});
 
-  const listBuilder = (text) => {
-    const recipe = document.createElement("li");
-    recipe.innerHTML = text + ' <button onclick="deleteNote(this)">x</button>';
-    recipes.appendChild(recipe);
-  };
+const listBuilder = (text) => {
+  const recipe = document.createElement("li");
+  recipe.innerHTML = text + ' <button onclick="deleteRecipe(this)">x</button>';
+  recipes.appendChild(recipe);
+};
 
-  const getNotes = JSON.parse(localStorage.getItem("recipes"));
+const getRecipe = JSON.parse(localStorage.getItem("recipes"));
 //getNotes.forEach((recipe) => {
- // listBuilder(recipe);
+//listBuilder(recipe);
 //});
 
 const deleteNote = (btn) => {
-    let el = btn.parentNode;
-    const index = [...el.parentElement.children].indexOf(el);
-    recipeStorage.splice(index, 1);
-    localStorage.setItem("recipes", JSON.stringify(recipeStorage));
-    el.remove();
-  };
+  let el = btn.parentNode;
+  const index = [...el.parentElement.children].indexOf(el);
+  recipeStorage.splice(index, 1);
+  localStorage.setItem("recipes", JSON.stringify(recipeStorage));
+  el.remove();
+  // this seems to work kind of. it will display if i put it in the console but not without console. i want to beable to hit save and it saves to local storage AND displays a list in Cookbook html 
+  for (var i = 0; i < localStorage.length; i++) {
+    alert(localStorage.getItem(localStorage.key(i)));
+    cookBook.addEventListener("submit");
+  }
+};
+
+
